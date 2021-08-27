7f804a0a043b47f149586d2a765f4a790ab49078
Nathan van Beelen <nathan@vanbeelen.org>
2021-08-27 11:15:00 +0200

    Add provisional RNN model for testing purposes

--- project_code/networks/rnn.py
+++ project_code/networks/rnn.py
@@ -1,32 +1,71 @@
-## Generic skeleton for RNN.
+import numpy as np
+import torch
 import torch.nn as nn
 
+import pytorch_lightning as pl
+
 # Define a Recurrent Neural Network
-class MutationNet(nn.Module):
+class MutationNet(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
         # Define the structure of the network
-        pass
+        # Note: this is a very provisional model for testing purposes
+        self.lstm = nn.LSTM(input_size = 11, 
+                            hidden_size = 20,
+                            num_layers = 1,
+                            batch_first=True)
+        self.bceloss = nn.BCELoss()
 
-    def forward(self, x):
+    def forward(self, x, hiddens=None):
         # Do the forward pass.
-        pass
 
-    def configure_optimizers(self):
-        # Return an optimizer (i.e. Adam).
-        pass
+        # TODO: We limit the sequence to 500 as it will result in NaNs
+        # in long(er) sequences. This bug should be fixed.
+        x = x[:, :500]
+
+        if hiddens == None:
+            out, hiddens = self.lstm(x)
+        else:
+            out, hiddens = self.lstm(x, hiddens)
+
+        return torch.sigmoid(out), hiddens
 
     def training_step(self, train_batch, batch_idx):
-        # Load the data from the train batch.
-        # Predict y_hat.
-        # Calculate the loss between y_hat and y.
-        # Log the loss with self.log('train_loss', loss).
-        # Return the loss.
-        pass
+        # Calculates the loss for a batch.
+        x = train_batch['behavioural_data']
+
+        y_hat, _ = self.forward(x)
+
+        if train_batch['treatment'] == 'None':
+            y = torch.ones(y_hat.shape, dtype=torch.double)    
+        else:
+            y = torch.zeros(y_hat.shape, dtype=torch.double)    
+        loss = self.bceloss(y_hat.double(), y.double())
+
+        # Log to TensorBoard.
+        self.log('train_loss', loss)
+
+        return loss
 
     def validation_step(self, val_batch, batch_idx):
         # The same as in training_step,
         # but now we only log the loss and don't return it:
-        # self.log('val_loss', loss)
-        pass
+        x = val_batch['behavioural_data']
+        y_hat, _ = self.forward(x)
+
+        if val_batch['treatment'] == 'None':
+            y = torch.ones(y_hat.shape, dtype=torch.double)    
+        else:
+            y = torch.zeros(y_hat.shape, dtype=torch.double)    
+
+        loss = self.bceloss(y_hat, y)
+
+        # Log to TensorBoard
+        self.log('val_loss', loss)
+
+    def configure_optimizers(self):
+        # Return an optimizer (i.e. Adam).
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
